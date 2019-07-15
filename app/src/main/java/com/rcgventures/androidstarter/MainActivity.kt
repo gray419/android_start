38b5a0ce5b8f67b7b364e20f409561c92f63bc1a
Robert Gray <robert.gray@compass.com>
2019-07-15 16:03:50 -0400

    Refactor

--- app/src/main/java/com/rcgventures/androidstarter/MainActivity.kt
+++ app/src/main/java/com/rcgventures/androidstarter/MainActivity.kt
@@ -8,23 +8,21 @@ import okhttp3.*
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private val MOVIE_URL = "https://gist.githubusercontent.com/gray419/a0d58d14a6105c7f423d52210f723f4c/raw/c0ffb5c4ae3d8f417f3fd249f078e2f51ce02d35/movies.json"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 
-    override fun onStart() {
-        super.onStart()
+        setContentView(R.layout.activity_main)
 
         val client = OkHttpClient()
         val request = Request.Builder()
-            .url("https://gist.githubusercontent.com/gray419/a0d58d14a6105c7f423d52210f723f4c/raw/c0ffb5c4ae3d8f417f3fd249f078e2f51ce02d35/movies.json")
+            .url(MOVIE_URL)
             .build()
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
-                Log.i("MainActivity", e.message)
+                Log.i("MainActivity", e.message ?: "Error making request")
             }
 
             override fun onResponse(call: Call, response: Response) {
