2eb572dcd4c0d798c3a82f467f6443e0c2c5557e
Robert Gray <robert.gray@compass.com>
2019-07-15 16:18:47 -0400

    Use anko views

--- app/src/main/java/com/rcgventures/androidstarter/MainActivity.kt
+++ app/src/main/java/com/rcgventures/androidstarter/MainActivity.kt
@@ -3,9 +3,23 @@ package com.rcgventures.androidstarter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.google.gson.Gson
 import okhttp3.*
+import org.jetbrains.anko.*
 import java.io.IOException
+
+class MainActivityUI : AnkoComponent<MainActivity> {
+    override fun createView(ui: AnkoContext<MainActivity>): View {
+        return with(ui) {
+            relativeLayout {
+                textView("Hello, World").lparams {
+                    centerInParent()
+                }
+            }
+        }
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     private val MOVIE_URL = "https://gist.githubusercontent.com/gray419/a0d58d14a6105c7f423d52210f723f4c/raw/c0ffb5c4ae3d8f417f3fd249f078e2f51ce02d35/movies.json"
@@ -13,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        MainActivityUI().setContentView(this)
 
         val client = OkHttpClient()
         val request = Request.Builder()
