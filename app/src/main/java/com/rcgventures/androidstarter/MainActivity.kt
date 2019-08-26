a85b19a778e07b7fbe369eb494d1a5837df5dfe6
Robert Gray <robert.gray@compass.com>
2019-08-26 16:30:08 -0400

    Update UI

--- app/src/main/java/com/rcgventures/androidstarter/MainActivity.kt
+++ app/src/main/java/com/rcgventures/androidstarter/MainActivity.kt
@@ -1,22 +1,39 @@
 package com.rcgventures.androidstarter
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.serialization.Mapper
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
 import okhttp3.*
 import org.jetbrains.anko.*
+import org.jetbrains.anko.design.appBarLayout
+import org.jetbrains.anko.design.coordinatorLayout
+import org.jetbrains.anko.design.floatingActionButton
+import org.jetbrains.anko.sdk27.coroutines.onClick
 import java.io.IOException
 
-class MainActivityUI : AnkoComponent<MainActivity> {
-    override fun createView(ui: AnkoContext<MainActivity>): View {
+class MainActivityUI: AnkoComponent<Context> {
+    lateinit var fab: FloatingActionButton
+
+    override fun createView(ui: AnkoContext<Context>): View {
         return with(ui) {
-            relativeLayout {
-                textView("Hello, World").lparams {
-                    centerInParent()
+            coordinatorLayout {
+                relativeLayout {
+                    textView("Hello, World")
+                }.lparams {
+                    gravity = Gravity.CENTER
+                }
+                fab = floatingActionButton {
+                    imageResource = android.R.drawable.ic_dialog_email
+                }.lparams {
+                    gravity = Gravity.BOTTOM or Gravity.END
+                    margin = dip(16)
                 }
             }
         }
@@ -29,26 +46,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        MainActivityUI().setContentView(this)
+        val ui = MainActivityUI()
+        ui.setContentView(this)
 
         val client = OkHttpClient()
         val request = Request.Builder()
             .url(MOVIE_URL)
             .build()
 
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Log.i("MainActivity", e.message ?: "Error making request")
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                val jsonResponse = response.body()?.string() ?: ""
-                val json = Json(JsonConfiguration.Stable)
-                val movieResponse = json.parse(MovieResponse.serializer(), jsonResponse)
-                movieResponse.movies?.forEach {
-                    Log.i("MainActivity", "${it.name} - ${it.rating ?: 0.0}")
+        ui.fab.onClick {
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.i("MainActivity", e.message ?: "Error making request")
                 }
-            }
-        })
+
+                override fun onResponse(call: Call, response: Response) {
+                    val jsonResponse = response.body()?.string() ?: ""
+                    val json = Json(JsonConfiguration.Stable)
+                    val movieResponse = json.parse(MovieResponse.serializer(), jsonResponse)
+                    movieResponse.movies?.forEach {
+                        Log.i("MainActivity", "${it.name} - ${it.rating ?: 0.0}")
+                    }
+                }
+            })
+        }
     }
 }
